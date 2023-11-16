d3ee88f333609b73656ecd44cc1abb261594353a
fernandocalenzani <fernandocalenzani@gmail.com>
2023-11-15 22:24:44 -0300

    add multilayer neural network

--- app/src/nn_onelayer.py
+++ app/src/nn_onelayer.py
@@ -1,24 +1,37 @@
 import numpy as np
 
+xi = np.array([[0, 0],
+               [1, 0],
+               [0, 1],
+               [1, 1]]
+              )
+yi = np.array([[0], [1], [1], [0]])
+wi = [
+    [-0.424, -0.740, -0.961],
+    [0.358, -0.577, -0.469],
+]
+w_hide = [
+    [-0.017, -0.893, 0.148],
+]
+bias = [1, 0]
+epoch = 100
 
-def step_function(x):
-    return 1 if x >= 1 else 0
+
+def summation(x, w):
+    return np.dot(x, w)
 
 
-def perceptron(input_data, weights, bias):
-    activation = np.dot(input_data, weights) + bias
-    output = step_function(activation)
-    return output
+def activation_function(params):
+    return 1 / (1 + np.exp(-params))
 
 
-inputs = np.array([(0, 0), (0, 1), (1, 0), (1, 1)])
-weights = np.array([(0.5, 0.5), (0.5, 0.5), (0.5, 0.5), (0.5, 0.5)])
-bias = 0
-err = 100
-result = []
+def calc(x, y):
+    return x * y
 
-for i in range(0, len(inputs), 1):
-    output = perceptron(inputs[i], weights[i], bias)
-    result.append(output)
 
-print("Saída do perceptron:", result)
+for j in range(epoch):
+    xi_layer = xi
+    sum_synapse = summation(xi_layer, wi)
+    hide_layer = activation_function(sum_synapse)
+
+print('END')
